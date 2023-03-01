4e9141aaef3b4822c8e9d04cf2137139a3a968a6
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-28 23:51:55 -0800

    Fix betty errors

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,7 +4,7 @@
  * binary_tree_depth - Measures the depth of a node in a binary tree
  * @tree: Pointer to the node to measure the depth
  *
- * Returns: Depth of the binary tree
+ * Return: Depth of the binary tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		left = binary_tree_height(tree->left);
 		right = binary_tree_height(tree->right);
-		
+
 		if (!(tree->left || tree->right))
 			return (0);
 
