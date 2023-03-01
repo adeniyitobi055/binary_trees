63d0efc6999bea6e9e674dffff686c453848d080
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-01 01:38:51 -0800

    check

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,20 +11,19 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 
-	if (tree)
+	if (!tree)
+		return (0);
+
+	if (!(tree->left && tree->right))
 	{
-		if (!(tree->left && tree->right))
-		{
-			leaves++;
-		}
-		else
-		{
-			if (tree->left)
-				binary_tree_leaves(tree->left);
-			if (tree->right)
-				binary_tree_leaves(tree->right);
-		}
-		return (leaves++);
+		leaves++;
+	}
+	else
+	{
+		if (tree->left)
+			binary_tree_leaves(tree->left);
+		if (tree->right)
+			binary_tree_leaves(tree->right);
 	}
 	return (0);
 }
